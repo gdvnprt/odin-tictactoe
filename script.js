7c1e6178dc939aae0a5ae7b1c1bfd08b915c3aac
Gavin Davenport <gavinkdavenport@gmail.com>
2024-02-16 16:59:21 -0800

    Estabilsh basic guidelines
    have a simple gameboard to reference and pseudocode for game logic

---
+++ script.js
@@ -0,0 +1,7 @@
+// build a gameboard object with 9 squares and the overall game state
+// build two player objects with names and a marker X or O
+// have the players take turns marking a square with their marker
+// if a player gets 3 of their markers in a row in any direction, report they win
+// if all squares fill with markers and there is no winner, report a tie
+// reset the board with a command
+// change player names with a command
