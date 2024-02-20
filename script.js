3fdc07346405f3952f65d285fdf287179fe26ea9
Gavin Davenport <gavinkdavenport@gmail.com>
2024-02-20 15:55:30 -0800

    Add players into board object
    players now part of object and game condition checks

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // build a gameboard array with 9 squares and the overall game state
 function createGameSquare(a) {
-    const squareNumber = a;
     let marker;
-    return { squareNumber, marker };
+    return { marker };
 };
 
 function createPlayer(name, marker) {
@@ -11,28 +10,28 @@ function createPlayer(name, marker) {
     return { name, mark };
 };
 
-const player1 = createPlayer(n, 'X');
-const player2 = createPlayer(n, 'O');
-
 function gameBoard() {
     let board = [];
     for (let i = 1; i < 10; i++) {
         board.push(createGameSquare(i));
     };
 
-    const markX = (a) => board[a].marker = 'X';
-    const markO = (b) => board[b].marker = 'O';
+    const player1 = (n) => createPlayer(n, 'X');
+    const player2 = (n) => createPlayer(n, '0');
 
-    const checkWinner = function(c) {
-        if ((board[0].marker == board[1].marker == board[2].marker == c) ||
-            (board[3].marker == board[4].marker == board[5].marker == c) ||
-            (board[6].marker == board[7].marker == board[8].marker == c) ||
-            (board[0].marker == board[3].marker == board[6].marker == c) ||
-            (board[1].marker == board[4].marker == board[7].marker == c) ||
-            (board[2].marker == board[5].marker == board[8].marker == c) ||
-            (board[0].marker == board[4].marker == board[8].marker == c) ||
-            (board[2].marker == board[4].marker == board[6].marker == c)) {
-                return c + ' is the winner!'
+    const markX = (a) => board[a].marker = 'X';
+    const markO = (a) => board[a].marker = 'O';
+
+    const checkWinner = function(player) {
+        if ((board[0].marker == board[1].marker == board[2].marker == player.mark) ||
+            (board[3].marker == board[4].marker == board[5].marker == player.mark) ||
+            (board[6].marker == board[7].marker == board[8].marker == player.mark) ||
+            (board[0].marker == board[3].marker == board[6].marker == player.mark) ||
+            (board[1].marker == board[4].marker == board[7].marker == player.mark) ||
+            (board[2].marker == board[5].marker == board[8].marker == player.mark) ||
+            (board[0].marker == board[4].marker == board[8].marker == player.mark) ||
+            (board[2].marker == board[4].marker == board[6].marker == player.mark)) {
+                return player + ' is the winner!'
             };
     };
 
@@ -50,10 +49,9 @@ function gameBoard() {
             };
     };
 
-    return { board, markX, markO, checkWinner, checkTie };
+    return { board, player1, player2, markX, markO, checkWinner, checkTie };
 };
 
 
-// if all squares fill with markers and there is no winner, report a tie
 // reset the board with a command
 // change player names with a command
