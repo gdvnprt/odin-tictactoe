5bdbe51e6e4d0b212b657ce9245d14946a599739
Gavin Davenport <gavinkdavenport@gmail.com>
2024-02-20 14:40:03 -0800

    Create game board square function
    factory function to output 9 squares to array with identifiers and marker state

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// build a gameboard object with 9 squares and the overall game state
+// build a gameboard array with 9 squares and the overall game state
+let gameBoard = [];
+
+function createGameSquare(a) {
+    const squareNumber = a;
+    let squareMarker = '';
+    return { squareNumber, squareMarker };
+};
+
+for (let i = 1; i < 10; i++) {
+    gameBoard.push(createGameSquare(i));
+};
+
 // build two player objects with names and a marker X or O
 // have the players take turns marking a square with their marker
 // if a player gets 3 of their markers in a row in any direction, report they win
