51f92e44e3dfa8d21a1950dc3215ff3a336548a9
Gavin Davenport <gavinkdavenport@gmail.com>
2024-02-20 15:34:57 -0800

    Build more of game object
    changing square markers and checking winners now part of game object

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // build a gameboard array with 9 squares and the overall game state
-let gameBoard = [];
-
 function createGameSquare(a) {
     const squareNumber = a;
-    let squareMarker = '';
-    return { squareNumber, squareMarker };
+    let marker;
+    return { squareNumber, marker };
 };
 
-for (let i = 1; i < 10; i++) {
-    gameBoard.push(createGameSquare(i));
+function gameBoard() {
+    let board = [];
+    for (let i = 1; i < 10; i++) {
+        board.push(createGameSquare(i));
+    };
+    const markX = (a) => board[a].marker = 'X';
+    const markO = (b) => board[b].marker = 'O';
+
+    const checkWinner = function(c) {
+        if ((board[0].marker == board[1].marker == board[2].marker == c) ||
+            (board[3].marker == board[4].marker == board[5].marker == c) ||
+            (board[6].marker == board[7].marker == board[8].marker == c) ||
+            (board[0].marker == board[3].marker == board[6].marker == c) ||
+            (board[1].marker == board[4].marker == board[7].marker == c) ||
+            (board[2].marker == board[5].marker == board[8].marker == c) ||
+            (board[0].marker == board[4].marker == board[8].marker == c) ||
+            (board[2].marker == board[4].marker == board[6].marker == c)) {
+                return c + ' is the winner!'
+            };
+    };
+
+    const checkTie = function() {
+        
+    }
+
+    return { board, markX, markO, checkWinner };
 };
 
 // build two player objects with names and a marker X or O
@@ -19,8 +41,10 @@ function createPlayer(name, marker) {
     return { name, marker };
 };
 
-// have the players take turns marking a square with their marker
-// if a player gets 3 of their markers in a row in any direction, report they win
+const player1 = createPlayer(n, 'X');
+const player2 = createPlayer(n, 'O')
+
+
 // if all squares fill with markers and there is no winner, report a tie
 // reset the board with a command
 // change player names with a command
